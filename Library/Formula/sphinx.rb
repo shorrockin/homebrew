f38daf80a26c4ee1978f333a14d3ca836eb85b60
Chris Shorrock <chris.shorrock@gmail.com>
2010-08-23 11:16:03 -0700

    updated md5

--- Library/Formula/sphinx.rb
+++ Library/Formula/sphinx.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Sphinx <Formula
   url 'http://www.sphinxsearch.com/downloads/sphinx-0.9.8.1.tar.gz'
   homepage 'http://www.sphinxsearch.com'
-  #md5 '7b9b618cb9b378f949bb1b91ddcc4f54'
+  md5 '428a14df41fb425e664d9e2d6178c037'
 
   def install
     fails_with_llvm "fails with: ld: rel32 out of range in _GetPrivateProfileString from /usr/lib/libodbc.a(SQLGetPrivateProfileString.o)"
